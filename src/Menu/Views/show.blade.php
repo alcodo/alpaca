552b6c2b906d019b1b1aecae32b93ecaa2a89800
approached <alexejkloos@gmail.com>
2016-08-05 20:16:22 +0200

    menu style

--- src/Menu/Views/show.blade.php
+++ src/Menu/Views/show.blade.php
@@ -1,8 +1,8 @@
-<p class="nav-title">{{ $menu->title }}</p>
+<p class="block-title">{{ $menu->title }}</p>
 @if($menu->items)
-    <ul class="nav nav-pills nav-stacked {{ $menu->class }}">
+    <ul class="block-list {{ $menu->class }}">
         @foreach ($menu->items as $item)
-            <li class="{{ isActiveUrl($item->href) }}">
+            <li class="block-item {{ isActiveUrl($item->href) }}">
                 {!! $item->getLink() !!}
             </li>
         @endforeach
