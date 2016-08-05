0b3ff0fec6353d54ac833fd28f110ff5f0e36e02
approached <alexejkloos@gmail.com>
2016-08-05 23:10:23 +0200

    * block style

--- src/Menu/Views/show.blade.php
+++ src/Menu/Views/show.blade.php
@@ -1,8 +1,8 @@
 <p class="block-title">{{ $menu->title }}</p>
 @if($menu->items)
-    <ul class="block-list {{ $menu->class }}">
+    <ul class="block {{ $menu->class }}">
         @foreach ($menu->items as $item)
-            <li class="block-item {{ isActiveUrl($item->href) }}">
+            <li class="{{ isActiveUrl($item->href) }}">
                 {!! $item->getLink() !!}
             </li>
         @endforeach
