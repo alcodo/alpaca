aaa23bb9a49eb602ecb3d0cd47f8daa2e49e7ebb
Alexej Kloos <alexejkloos@gmail.com>
2016-10-07 10:12:20 +0200

    * gallery routes 5.3

--- src/Gallery/routes.php
+++ src/Gallery/routes.php
@@ -1,3 +1,9 @@
 <?php
 
-Route::resource('/backend/gallery', 'GalleryBackend');
+if (isLaravelVersion5_3()) {
+    Route::group(['as' => 'backend.'], function () {
+        Route::resource('/backend/gallery', 'GalleryBackend');
+    });
+} else {
+    Route::resource('/backend/gallery', 'GalleryBackend');
+}
