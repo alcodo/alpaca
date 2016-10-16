f14018b5e81f5bdcc7042c581ec5585c198f8eb3
approached <alexejkloos@gmail.com>
2016-10-16 15:24:43 +0200

    * CookieConsent view

--- src/CookieConsent/Views/bar.blade.php
+++ src/CookieConsent/Views/bar.blade.php
@@ -2,13 +2,24 @@
 
 <div class="cookieconsent">
     <div class="container">
-        <div class="cookieconsent-text pull-left">
-            {{ trans('cookieconsent::bar.description') }}
-            <a href="{{ config('cookieconsent.link') }}" target="_blank">
-                {{ trans('cookieconsent::bar.more') }}
-            </a>
+        <div class="row">
+            <div class="col-sm-11 col-xs-10">
+                {{ trans('cookieconsent::bar.description') }}
+                <a href="{{ config('cookieconsent.link') }}" target="_blank">
+                    {{ trans('cookieconsent::bar.more') }}
+                </a>
+            </div>
+            <div class="col-sm-1 col-xs-2">
+                <button class="btn btn-sm btn-warning pull-right cookieconsent-close">OK</button>
+            </div>
         </div>
-        <button class="btn btn-sm btn-warning pull-right cookieconsent-close">OK</button>
+        {{--<div class="cookieconsent-text pull-left">--}}
+            {{--{{ trans('cookieconsent::bar.description') }}--}}
+            {{--<a href="{{ config('cookieconsent.link') }}" target="_blank">--}}
+                {{--{{ trans('cookieconsent::bar.more') }}--}}
+            {{--</a>--}}
+        {{--</div>--}}
+        {{--<button class="btn btn-sm btn-warning pull-right cookieconsent-close">OK</button>--}}
     </div>
 </div>
 
