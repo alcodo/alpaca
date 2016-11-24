325d4f95732445084d39b9c12ec56838cd255417
approached <alexejkloos@gmail.com>
2016-11-24 22:17:56 +0100

    reset password subject

--- src/User/Notifications/ResetPassword.php
+++ src/User/Notifications/ResetPassword.php
@@ -45,6 +45,8 @@ class ResetPassword extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
+            ->subject(trans('user::user.reset_password'))
+            ->line(trans('user::user.you_receive_email_because'))
             ->line(trans('user::user.you_receive_email_because'))
             ->action(trans('user::user.reset_password'), url('password/reset', $this->token))
             ->line(trans('user::user.if_not_you_requested'));
