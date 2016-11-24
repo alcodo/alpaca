1d5c02593e2856c889214bf3ac6ec0ccfc6fc82c
approached <alexejkloos@gmail.com>
2016-11-24 19:46:37 +0100

    email routes

--- src/Email/Views/cmf/menu.blade.php
+++ src/Email/Views/cmf/menu.blade.php
@@ -1,5 +1,5 @@
 <li class="block-item {{ isActiveRoute('backend.email.index') }}">
-    <a href="{{ route('backend.gallery.index') }}">
+    <a href="{{ route('backend.email.index') }}">
         {{ trans('email::email.email-templates') }}
     </a>
 </li>
--- src/Email/routes.php
+++ src/Email/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['as' => 'backend.'], function () {
-    Route::get('/backend/email', 'EmailController@index');
-    Route::get('/backend/email/register', 'EmailController@register');
-    Route::get('/backend/email/passwort_reset', 'EmailController@passwort_reset');
+    Route::get('/backend/email', 'EmailController@index')->name('email.index');
+    Route::get('/backend/email/register', 'EmailController@register')->name('email.register');
+    Route::get('/backend/email/passwort_reset', 'EmailController@passwort_reset')->name('email.passwort_reset');
 });
