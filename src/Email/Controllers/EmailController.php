2116ee324e3939cda44f008e3ea64bb5659d944a
Alexej Kloos <alexejkloos@gmail.com>
2016-11-24 15:34:27 +0100

    register email template viewable

--- src/Email/Controllers/EmailController.php
+++ src/Email/Controllers/EmailController.php
@@ -25,13 +25,22 @@ class EmailController extends Controller
 
     public function register()
     {
-        $user = Auth::user();
-        return view('user::emails.verification', compact('user'));
+        $mail = new \Illuminate\Auth\Notifications\VerifyAccount('SECRET_TOKEN', 'JOHNdoe');
+        return $this->generateNotification($mail);
     }
 
     public function passwort_reset()
     {
         $mail = new \Illuminate\Auth\Notifications\ResetPassword('SECRET_TOKEN');
+        return $this->generateNotification($mail);
+    }
+
+    /**
+     * @param $mail
+     * @return mixed
+     */
+    protected function generateNotification($mail)
+    {
         $message = $mail->toMail(null);
 
         $template = $message->view[0];
