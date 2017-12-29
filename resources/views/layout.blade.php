9c5dad266ef6624ae96f30d95336675cb47b56d4
Alexej Kloos <alexejkloos@gmail.com>
2017-12-29 12:45:53 +0100

    copied layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,1 +1,60 @@
-Hallo
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+    <style>
+    </style>
+
+    <!-- Bootstrap CSS -->
+    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"--}}
+    {{--integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">--}}
+</head>
+<body>
+
+<div id="app">
+
+    @include('navbar.navbar')
+
+    {{--<br>--}}
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                @include('area.left')
+                <br>
+                @include('area.left')
+                <br>
+                @include('area.left')
+            </div>
+            <div class="col-sm-9">
+                @include('area.content')
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        @include('area.footer')
+    </footer>
+
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+{{--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"--}}
+{{--integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"--}}
+{{--crossorigin="anonymous"></script>--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"--}}
+{{--integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"--}}
+{{--crossorigin="anonymous"></script>--}}
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"--}}
+{{--integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"--}}
+{{--crossorigin="anonymous"></script>--}}
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
