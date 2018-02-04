846c92c8e6544eda6c17727ea55e2a5d6bd5ae08
Alexej Kloos <alexejkloos@gmail.com>
2018-02-04 14:46:48 +0100

    * alpaca block per permission

--- src/Listeners/AlpacaBlockListener.php
+++ src/Listeners/AlpacaBlockListener.php
@@ -25,7 +25,11 @@ class AlpacaBlockListener
      */
     public function handle()
     {
-//        if (Auth::check() && Auth::user() && Auth::user()->hasRole('admin')) {
+        // user is not logged in
+        if (Auth::check() === false) {
+            return null;
+        }
+
         $block = new Block([
             'title' => 'Alpaca administration',
             'slug' => 'alpaca-administration',
@@ -44,22 +48,73 @@ class AlpacaBlockListener
 
 
         $block->menu = new Menu([
-            'title' => 'fodsfsfo',
+            'title' => 'nav',
         ]);
+        $block->menu->links = collect();
 
-        $block->menu->links = collect([
-            new MenuLink(['text' => 'Page', 'title' => 'Page administration', 'href' => '/backend/page',]),
-            new MenuLink(['text' => 'Category', 'title' => 'Category administration', 'href' => '/backend/category',]),
-            new MenuLink(['text' => 'Menu', 'title' => 'Menu administration', 'href' => '/backend/menu',]),
-            new MenuLink(['text' => 'Block', 'title' => 'Block administration', 'href' => '/backend/block',]),
-            new MenuLink(['text' => 'User', 'title' => 'User administration', 'href' => '/backend/user',]),
-            new MenuLink(['text' => 'Roles', 'title' => 'Roles administration', 'href' => '/backend/role',]),
-            new MenuLink(['text' => 'Permission', 'title' => 'Permission administration', 'href' => '/backend/permission',]),
-            new MenuLink(['text' => 'Image', 'title' => 'Image administration', 'href' => '/backend/image',]),
-            new MenuLink(['text' => 'E-Mail Templates', 'title' => 'E-Mail Templates', 'href' => '/backend/email-template',]),
-        ]);
+
+        /**
+         * Add permission links
+         */
+        $user = Auth::user();
+        if ($user->can('user.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Page', 'title' => 'Page administration', 'href' => '/backend/page',])
+            );
+        }
+
+        if ($user->can('category.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Category', 'title' => 'Category administration', 'href' => '/backend/category',])
+            );
+        }
+
+        if ($user->can('menu.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Menu', 'title' => 'Menu administration', 'href' => '/backend/menu',])
+            );
+        }
+
+        if ($user->can('block.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Block', 'title' => 'Block administration', 'href' => '/backend/block',])
+            );
+        }
+
+        if ($user->can('user.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'User', 'title' => 'User administration', 'href' => '/backend/user',])
+            );
+        }
+
+        if ($user->can('role.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Roles', 'title' => 'Roles administration', 'href' => '/backend/role',])
+            );
+        }
+
+        if ($user->can('permission.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Permission', 'title' => 'Permission administration', 'href' => '/backend/permission',])
+            );
+        }
+
+        if ($user->can('image.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'Image', 'title' => 'Image administration', 'href' => '/backend/image',])
+            );
+        }
+
+        if ($user->can('emailtemplates.administer')) {
+            $block->menu->links->push(
+                new MenuLink(['text' => 'E-Mail Templates', 'title' => 'E-Mail Templates', 'href' => '/backend/email-template',])
+            );
+        }
+
+        if ($block->menu->links->isEmpty()) {
+            return null;
+        }
 
         return $block;
-//        }
     }
 }
