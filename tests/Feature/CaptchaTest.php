3f33e6d13adbb3a3cc2dc0e31b2b540765f4885b
Alexej Kloos <alexejkloos@gmail.com>
2018-03-28 21:09:40 +0200

    * captcha test

--- tests/Feature/CaptchaTest.php
+++ tests/Feature/CaptchaTest.php
@@ -11,7 +11,7 @@ class SitemapTest extends IntegrationTest
     public function testRender()
     {
         $captcha = new CaptchaBuilder('a', 'b');
-        $this->assertEquals('<script src="https://www.google.com/recaptcha/api.js&hl=en" async defer></script>', $captcha->renderJs());
+        $this->assertEquals('<script src="https://www.google.com/recaptcha/api.js?hl=en" async defer></script>', $captcha->renderJs());
         $this->assertEquals('<div class="g-recaptcha" data-sitekey="a"></div>', $captcha->renderInput());
     }
 
