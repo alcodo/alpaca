002eb3a45b02dd34c2ef0179886c0f5954fec4d2
Alexej Kloos <alexejkloos@gmail.com>
2018-03-28 19:30:47 +0000

    Apply fixes from StyleCI

--- src/AlpacaServiceProvider.php
+++ src/AlpacaServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace Alpaca;
 
-use Alpaca\Support\Captcha\CaptchaBuilder;
 use Alpaca\Support\Permission\Guard;
-use Alpaca\Support\Captcha\CaptchaFacade;
 use Alpaca\Support\Block\BlockFacade;
-use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Support\Facades\Event;
 use Alpaca\Events\Page\PageWasCreated;
 use Alpaca\Events\Page\PageWasDeleted;
@@ -25,8 +22,11 @@ use Alpaca\Events\Image\ImageWasUpdated;
 use Alpaca\Events\Block\BlockIsRequested;
 use Alpaca\Listeners\AlpacaBlockListener;
 use Alpaca\Listeners\User\IsUserVerified;
+use Alpaca\Support\Captcha\CaptchaFacade;
 use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Auth\Events\PasswordReset;
 use Alpaca\Listeners\User\AssignGuestRole;
+use Alpaca\Support\Captcha\CaptchaBuilder;
 use Alpaca\Events\Sitemap\SitemapIsRequested;
 use Alpaca\Listeners\User\AssignRegisterRole;
 use Alpaca\Commands\PublishTranslationCommand;
