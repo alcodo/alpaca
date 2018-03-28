3298d1205677c0ee80d00946b5e646372460e6c8
Alexej Kloos <alexejkloos@gmail.com>
2018-03-28 15:13:30 +0000

    Apply fixes from StyleCI

--- src/Support/Captcha/CaptchaBuilder.php
+++ src/Support/Captcha/CaptchaBuilder.php
@@ -4,7 +4,6 @@ namespace Alpaca\Support\Captcha;
 
 class CaptchaBuilder
 {
-
     private $publicKey;
     private $secretKey;
 
@@ -13,7 +12,6 @@ class CaptchaBuilder
         $this->publicKey = $publicKey;
         $this->secretKey = $secretKey;
     }
-
 
     public function renderJs()
     {
@@ -31,7 +29,7 @@ class CaptchaBuilder
     {
         $request = request();
 
-        if (!$request->has('g-recaptcha-response')) {
+        if (! $request->has('g-recaptcha-response')) {
             return false;
         }
 
@@ -47,11 +45,10 @@ class CaptchaBuilder
         $response = json_decode($response->getBody(), true);
 
         // check
-        if (!isset($response['success']) || $response['success'] !== true) {
+        if (! isset($response['success']) || $response['success'] !== true) {
             return false;
         }
 
         return true;
     }
-
 }
