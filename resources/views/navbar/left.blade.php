0a05816a557ed7d30cec7c6b8e9219a27a6b213d
Alexej Kloos <alexejkloos@gmail.com>
2018-04-23 19:42:22 +0200

    * left menu

--- resources/views/navbar/left.blade.php
+++ resources/views/navbar/left.blade.php
@@ -2,4 +2,14 @@
     <li class="nav-item {{ isActiveUrlExact('/') }}">
         <a class="nav-link" href="/">Home</a>
     </li>
+    <li class="nav-item {{ isActiveUrl('/about-us') }}">
+        <a class="nav-link" href="/about-us">About us</a>
+
+        {{--Block for mobile view--}}
+        @if(isActiveUrl('/about-us') && !empty(Block::getMobileMenuBlocks()))
+            <div class="d-sm-block d-md-none" style="font-size: 90%;background-color: #e6e6e6;padding: 10px;">
+                {!! Block::getMobileMenuBlocks() !!}
+            </div>
+        @endif
+    </li>
 </ul>
